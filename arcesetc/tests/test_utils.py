760bb6f3e73ac178ea7dcf6ad9998ce90e8a5c5c
Brett Morris <brettmorris21@gmail.com>
2018-12-11 08:32:41 -0800

    Updating tests

--- arcesetc/tests/test_utils.py
+++ arcesetc/tests/test_utils.py
@@ -12,21 +12,24 @@ from ..util import (reconstruct_order, closest_sptype, archive, scale_flux,
 path = os.path.dirname(__file__)
 
 
-@pytest.mark.parametrize("order", [35, 41, 60])
+@pytest.mark.parametrize("order", [30, 35, 41, 60, 65, 70, 75, 80, 90])
 def test_reconstruct_order(order):
     """
     End-to-end functional test on several well-behaved orders of an early-type
     star.
     """
 
-    fits_path = os.path.join(path, os.pardir, 'data', 'HR3454.0016.wfrmcpc.fits')
+    fits_path = os.path.join(path, os.pardir, 'data',
+                             'HR5191.0002.wfrmcpc.fits')
 
     b3v = readspec.read_fits_spectrum1d(fits_path)
     header = fits.getheader(fits_path)
 
+    # Reconstruct the order for a star with the same V mag as the template
+
     wave, flux, sp_type, exp_time = reconstruct_order('B3V',
                                                       b3v[order].wavelength.mean(),
-                                                      4.3,
+                                                      1.86,
                                                       exp_time=header['EXPTIME']*u.s)
 
     interp_flux = np.interp(b3v[order].wavelength, wave, flux)
@@ -41,7 +44,7 @@ def test_closest_sptype():
 
 def test_scale_flux():
     """
-    Check that the flux scaling is workign appropriately by inputting the actual
+    Check that the flux scaling is working appropriately by inputting the actual
     magnitude of a particular star, show that it returns flux scaling == 1
     """
     assert np.abs(scale_flux(archive['HR 3454'], V=4.3) - 1) < 1e-6
